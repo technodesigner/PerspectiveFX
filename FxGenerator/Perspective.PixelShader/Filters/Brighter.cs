ad93c81ec6f0cb33705137a7c74567715b13f527
odewit_cp <SND\odewit_cp>
2009-08-20 20:41:31 +0000

    2 new filters : Brighter, to change the brightness, and Saturator, to change the saturation level.

---
+++ FxGenerator/Perspective.PixelShader/Filters/Brighter.cs
@@ -0,0 +1,49 @@
+﻿//------------------------------------------------------------------
+//
+//  For licensing information and to get the latest version go to:
+//  http://www.codeplex.com/perspectivefx
+//
+//  THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY
+//  OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
+//  LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR
+//  FITNESS FOR A PARTICULAR PURPOSE.
+//
+//------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Perspective.PixelShader.Filters
+{
+    /// <summary>
+    /// A filter to change the brightness of an image.
+    /// The default level (1.0) doesn't affect the image.
+    /// Generates the HLSL code for a WPF or Silverlight effect.
+    /// </summary>
+    public class Brighter : FilterBase
+    {
+        EffectProperty _level;
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public Brighter()
+        {
+            _level = new EffectProperty(this);
+            _level.Name = "Level";
+            _level.WrapperType = typeof(double);
+            _level.DefaultValue = "1.0";
+            Properties.Add(_level);
+        }
+
+        /// <summary>
+        /// Generates the HLSL body.
+        /// </summary>
+        /// <param name="output">The writer object for code generation.</param>
+        public override void MainHlsl(System.CodeDom.Compiler.IndentedTextWriter output)
+        {
+            output.WriteLine("color.rgb *= {0};", this._level.FullName);
+        }
+    }
+}
---
+++ FxGenerator/Perspective.PixelShader/Filters/Saturator.cs
@@ -0,0 +1,55 @@
+﻿//------------------------------------------------------------------
+//
+//  For licensing information and to get the latest version go to:
+//  http://www.codeplex.com/perspectivefx
+//
+//  THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY
+//  OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
+//  LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR
+//  FITNESS FOR A PARTICULAR PURPOSE.
+//
+//------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Perspective.PixelShader.Filters
+{
+    /// <summary>
+    /// A filter to change the saturation of an image.
+    /// The default level (1.0) doesn't affect the image.
+    /// A level of 0.0 gives a greyscale image.
+    /// A negative level gives a negative image.
+    /// Generates the HLSL code for a WPF or Silverlight effect.
+    /// </summary>
+    public class Saturator : FilterBase
+    {
+        EffectProperty _level;
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public Saturator()
+        {
+            _level = new EffectProperty(this);
+            _level.Name = "Level";
+            _level.WrapperType = typeof(double);
+            _level.DefaultValue = "1.0";
+            Properties.Add(_level);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="output"></param>
+        public override void MainHlsl(System.CodeDom.Compiler.IndentedTextWriter output)
+        {
+            // see http://en.wikipedia.org/wiki/Luma_(video)
+            output.WriteLine("float luminance = dot(color.rgb, float3(0.2126, 0.7152, 0.0722));");
+            
+            // The result is the interpolation between luminance and chromatic (rgb) image
+            output.WriteLine("color.rgb = lerp(luminance.xxx, color.rgb, {0});", this._level.FullName);
+        }
+    }
+}
